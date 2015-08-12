07c5d7b7b73a3148641acc308d0e63c31e2ba598
Les Orchard <me@lmorchard.com>
2015-08-12 15:55:17 -0400

    Add some icons and a toolbar button

--- sub/index.js
+++ sub/index.js
@@ -7,3 +7,21 @@ function dummy(text, callback) {
 }
 
 exports.dummy = dummy;
+
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+
+var button = buttons.ActionButton({
+  id: "idea-town-link",
+  label: "Idea Town",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick(state) {
+  tabs.open("http://developer.mozilla.org/");
+}
