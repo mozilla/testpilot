179e421c3a5cf34dc23df8dc79d4ec608eb68f4f
Les Orchard <me@lmorchard.com>
2015-08-18 14:10:30 -0400

    Move static assets managed by Gulp to make Django happier

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,9 @@ const connect = require('gulp-connect');
 
 const IS_DEBUG = true;
 
+const SRC_PATH = './client-src/';
+const DEST_PATH = './client-build/';
+
 // Lint the gulpfile
 gulp.task('selfie', function selfieTask() {
   return gulp.src('gulpfile.js')
@@ -29,7 +32,7 @@ gulp.task('selfie', function selfieTask() {
 
 // Lint the *.js files
 gulp.task('lint', function lintTask() {
-  return gulp.src(['*.js', 'static-src/scripts/**/*.js'])
+  return gulp.src(['*.js', SRC_PATH + 'scripts/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
@@ -37,30 +40,40 @@ gulp.task('lint', function lintTask() {
 
 gulp.task('clean', function cleanTask(done) {
   del([
-    'static'
+    DEST_PATH
   ], done);
 });
 
 gulp.task('npm:tabzilla:img', function npmTabzillaImgTask() {
   return gulp.src('node_modules/mozilla-tabzilla/media/**')
-    .pipe(gulp.dest('static/vendor/mozilla-tabzilla/media'));
+    .pipe(gulp.dest(DEST_PATH + 'vendor/mozilla-tabzilla/media'));
 });
 
+// Copy the tabzilla assets into the src dir for inclusion in minimization
 gulp.task('npm:tabzilla:css', function npmTabzillaCssTask() {
   return gulp.src('./node_modules/mozilla-tabzilla/css/tabzilla.css')
     .pipe(rename('mozilla-tabzilla/css/tabzilla.scss'))
-    .pipe(gulp.dest('./static-src/vendor'));
+    .pipe(gulp.dest(SRC_PATH + 'vendor'));
 });
 
+// Copy the normalize assets into the src dir for inclusion in minimization
 gulp.task('npm:normalize', function npmNormalizeTask() {
   return gulp.src('./node_modules/normalize.css/normalize.css')
     .pipe(rename('normalize.css/normalize.scss'))
-    .pipe(gulp.dest('./static-src/vendor'));
+    .pipe(gulp.dest(SRC_PATH + 'vendor'));
+});
+
+gulp.task('vendor', function vendorTask(done) {
+  return runSequence([
+    'npm:tabzilla:img',
+    'npm:tabzilla:css',
+    'npm:normalize'
+  ], done);
 });
 
 // Scripts
 gulp.task('scripts', function scriptsTask() {
-  const b = browserify('./static-src/scripts/main.js', {
+  const b = browserify(SRC_PATH + 'scripts/main.js', {
     debug: IS_DEBUG
   });
 
@@ -73,33 +86,25 @@ gulp.task('scripts', function scriptsTask() {
     .pipe(uglify())
     .on('error', gutil.log)
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./static/scripts'));
+    .pipe(gulp.dest(DEST_PATH + 'scripts'));
 });
 
 // Styles
 gulp.task('styles', function stylesTask() {
-  return gulp.src('./static-src/styles/main.scss')
+  return gulp.src(SRC_PATH + 'styles/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('static/styles'))
+    .pipe(gulp.dest(DEST_PATH + 'styles'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('static/styles'));
+    .pipe(gulp.dest(DEST_PATH + 'styles'));
 });
 
 // Images
 gulp.task('images', function imagesTask() {
-  return gulp.src('./static-src/images/**/*')
+  return gulp.src(SRC_PATH + 'images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('static/images'));
-});
-
-gulp.task('vendor', function vendorTask(done) {
-  return runSequence([
-    'npm:tabzilla:img',
-    'npm:tabzilla:css',
-    'npm:normalize'
-  ], done);
+    .pipe(gulp.dest(DEST_PATH + 'images'));
 });
 
 gulp.task('build', function buildTask(done) {
@@ -115,15 +120,15 @@ gulp.task('build', function buildTask(done) {
 
 // Watches the things
 gulp.task('watch', ['build'], function watchTask () {
-  gulp.watch('static-src/styles/**/*', ['styles']);
-  gulp.watch('static-src/images/**/*', ['images']);
-  gulp.watch('static-src/scripts/**/*', ['scripts']);
+  gulp.watch(SRC_PATH + 'styles/**/*', ['styles']);
+  gulp.watch(SRC_PATH + 'images/**/*', ['images']);
+  gulp.watch(SRC_PATH + 'scripts/**/*', ['scripts']);
 });
 
 // Set up a webserver for the static assets
 gulp.task('connect', function connectTask () {
   connect.server({
-    root: 'static',
+    root: DEST_PATH,
     livereload: false,
     port: 9988
   });
--- idea_town/settings.py
+++ idea_town/settings.py
@@ -92,6 +92,9 @@ USE_TZ = config('USE_TZ', default=True, cast=bool)
 
 STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'static'))
 STATIC_URL = config('STATIC_URL', '/static/')
+STATICFILES_DIRS = [
+    'client-build',  # Managed by gulpfile.js
+]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
