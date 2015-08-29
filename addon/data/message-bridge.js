a59a1732477536260fb76112d2b563f7bb4bef6d
Dave Justice <davejustishh@gmail.com>
2015-08-28 17:15:45 -0700

    fix #8: uninstall experiment, and event wrap router

--- addon/data/message-bridge.js
+++ addon/data/message-bridge.js
@@ -1,3 +1,11 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+/* global cloneInto */
+
 // Page script acts as messaging bridge between addon and web content.
 
 window.addEventListener('from-web-to-addon', function(event) {
--- addon/index.js
+++ addon/index.js
@@ -4,6 +4,8 @@
  * http://mozilla.org/MPL/2.0/.
  */
 
+let app;
+const store = require('sdk/simple-storage').storage;
 const {Cu} = require('chrome');
 const self = require('sdk/self');
 const tabs = require('sdk/tabs');
@@ -12,16 +14,26 @@ const {ActionButton} = require('sdk/ui/button/action');
 const AddonManager = Cu.import('resource://gre/modules/AddonManager.jsm').AddonManager;
 const prefs = require('sdk/simple-prefs').prefs;
 const updates = require('./package.json').UPDATES;
-const state = {updates: updates.map(function(u) {
-  return u.title;
-})};
+const Router = require('./route');
+const state = {updates: updates};
 
 const mod = new PageMod({
   include: prefs.ALLOWED_ORIGINS.split(','),
   contentScriptFile: self.data.url('message-bridge.js'),
   contentScriptWhen: 'start',
-  onAttach: setupWorker
+  onAttach: setupApp
 });
+
+require('sdk/system/unload').when(function(reason) {
+  if (reason === 'uninstall') {
+    if (store.experiments) {
+      uninstall(store.experiments);
+      delete store.experiments;
+    }
+  }
+});
+
+if (!store.experiments) store.experiments = [];
 
 const button = ActionButton({ // eslint-disable-line new-cap
   id: 'idea-town-link',
@@ -38,17 +50,64 @@ const button = ActionButton({ // eslint-disable-line new-cap
   }
 });
 
+function setupApp() {
+  app = new Router(mod);
 
-function sendToWeb(data) {
-  mod.port.emit('from-addon-to-web', data);
+  if (self.loadReason === 'install') {
+    app.send('addon-self:installed');
+  } else if (self.loadReason === 'enable') {
+    app.send('addon-self:enabled');
+  } else if (self.loadReason === 'upgrade') {
+    app.send('addon-self:upgraded');
+  }
+
+  app.on('update-check', function() {
+    app.send('addon-updates', state.updates);
+  });
+
+  app.on('update-approve', function(msg) {
+    const msgIds = msg.map(m => m.id);
+    triggerInstalls(updates.filter(u => require('array-includes')(msgIds, u.id)));
+  });
+
+  app.on('uninstall-all', function() {
+    uninstall(store.experiments);
+  });
+
+  app.on('uninstall', function(msg) {
+    uninstall(msg);
+  });
+
+  app.on('loaded', function() {
+    app.send('addon-available', {
+      experiments: store.experiments,
+      updates: state.updates
+    });
+  });
+}
+
+function uninstall(toUninstall) {
+  toUninstall.forEach(u => {
+    AddonManager.getAddonByID(u.id, function(a) {
+      a.uninstall();
+    });
+  });
+}
+
+function triggerInstalls(approvedUpdates) {
+  approvedUpdates.forEach(u => {
+    AddonManager.getInstallForURL(u.url, function(install) {
+      install.install();
+    }, 'application/x-xpinstall');
+  });
 }
 
 function formatInstallData(install, addon) {
   let formatted = {
-    'name': install.name ? install.name : '',
+    'name': install.name || '',
     'error': install.error,
     'state': install.state,
-    'version': install.version ? install.version : '',
+    'version': install.version || '',
     'progress': install.progress,
     'maxProgress': install.maxProgress
   };
@@ -67,90 +126,56 @@ function formatInstallData(install, addon) {
   return formatted;
 }
 
-function installAddon(data) {
-  AddonManager.getInstallForURL(data, function(install) {
-    install.install();
-  }, 'application/x-xpinstall');
-}
+AddonManager.addAddonListener({
+  onUninstalling: function(addon) {
+    app.send('addons-uninstalling', {
+      id: addon.id,
+      name: addon.name
+    });
+  },
+  onUninstalled: function(addon) {
+    app.send('addons-uninstalled', {
+      id: addon.id,
+      name: addon.name
+    });
 
-function filterUpdates(approvedUpdates) {
-  const filteredUpdates = [];
-  approvedUpdates.forEach(function(appUpdate) {
-    filteredUpdates.push(updates.find(function(update) {
-      return update.title === appUpdate;
-    }));
-  });
-
-  return filteredUpdates;
-}
-
-function triggerInstalls(approvedUpdates) {
-  filterUpdates(approvedUpdates).forEach(function(update) {
-    installAddon(update.url);
-  });
-}
-
-function setupWorker() {
-  if (self.loadReason === 'install') {
-    sendToWeb({type: 'addon-self:installed'});
-  } else if (self.loadReason === 'enable') {
-    sendToWeb({type: 'addon-self:enabled'});
-  } else if (self.loadReason === 'upgrade') {
-    sendToWeb({type: 'addon-self:upgraded'});
+    store.experiments = store.experiments.filter(ex => ex.id !== addon.id);
+    state.updates = require('exclude')(updates, store.experiments);
   }
+});
 
-  mod.port.on('from-web-to-addon', function(msg) {
-    if (!msg.type) { return; }
-    switch (msg.type) {
-      case 'update-check':
-        sendToWeb({ type: 'addon-updates', detail: state.updates });
-        break;
-      case 'update-approve':
-        state.updates = require('exclude')(state.updates, msg.detail);
-        button.badge = state.updates.length;
-        triggerInstalls(msg.detail);
-        break;
-      case 'loaded':
-        sendToWeb({ type: 'addon-available' });
-        break;
-      default:
-        sendToWeb({ type: 'echo', data: msg });
-        break;
-    }
-  });
-
-  const installListeners = {
-    onInstallEnded: function(install, addon) {
-      sendToWeb({type: 'addon-install:install-ended', detail: formatInstallData(install, addon)});
-    },
-    onInstallFailed: function(install) {
-      sendToWeb({type: 'addon-install:install-failed', detail: formatInstallData(install)});
-    },
-    onInstallStarted: function(install) {
-      sendToWeb({type: 'addon-install:install-started', detail: formatInstallData(install)});
-    },
-    onNewInstall: function(install) {
-      sendToWeb({type: 'addon-install:install-new', detail: formatInstallData(install)});
-    },
-    onInstallCancelled: function(install) {
-      sendToWeb({type: 'addon-install:install-cancelled', detail: formatInstallData(install)});
-    },
-    onDownloadStarted: function(install) {
-      sendToWeb({type: 'addon-install:download-started', detail: formatInstallData(install)});
-    },
-    onDownloadProgress: function(install) {
-      sendToWeb({type: 'addon-install:download-progress', detail: formatInstallData(install)});
-    },
-    onDownloadEnded: function(install) {
-      sendToWeb({type: 'addon-install:download-ended', detail: formatInstallData(install)});
-    },
-    onDownloadCancelled: function(install) {
-      sendToWeb({type: 'addon-install:download-cancelled', detail: formatInstallData(install)});
-    },
-    onDownloadFailed: function(install) {
-      sendToWeb({type: 'addon-install:download-failed', detail: formatInstallData(install)});
-    }
-  };
-
-  AddonManager.addInstallListener(installListeners);
-}
+AddonManager.addInstallListener({
+  onInstallEnded: function(install, addon) {
+    store.experiments.push(state.updates.find(u => u.id === addon.id));
+    state.updates = state.updates.filter(u => u.id !== addon.id);
+    button.badge = state.updates.length;
+    app.send('addon-install:install-ended', formatInstallData(install, addon));
+  },
+  onInstallFailed: function(install) {
+    app.send('addon-install:install-failed', formatInstallData(install));
+  },
+  onInstallStarted: function(install) {
+    app.send('addon-install:install-started', formatInstallData(install));
+  },
+  onNewInstall: function(install) {
+    app.send('addon-install:install-new', formatInstallData(install));
+  },
+  onInstallCancelled: function(install) {
+    app.send('addon-install:install-cancelled', formatInstallData(install));
+  },
+  onDownloadStarted: function(install) {
+    app.send('addon-install:download-started', formatInstallData(install));
+  },
+  onDownloadProgress: function(install) {
+    app.send('addon-install:download-progress', formatInstallData(install));
+  },
+  onDownloadEnded: function(install) {
+    app.send('addon-install:download-ended', formatInstallData(install));
+  },
+  onDownloadCancelled: function(install) {
+    app.send('addon-install:download-cancelled', formatInstallData(install));
+  },
+  onDownloadFailed: function(install) {
+    app.send('addon-install:download-failed', formatInstallData(install));
+  }
+});
