8ba5706c4beb8c53cc41d9e66d24b63c78accb6d
Les Orchard <me@lmorchard.com>
2015-10-16 15:57:55 -0400

    Remove unused views
    - Remove unused /accounts/ views
    
    - Remove unused home page view from base app
    
    closes #64, closes #65

--- idea_town/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def profile(request):
-    return render(request, 'idea_town/accounts/profile.html')
--- idea_town/base/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'idea_town/home.html')
