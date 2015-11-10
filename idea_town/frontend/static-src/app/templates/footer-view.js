4e91e251d36d0d1c4953b210396393eeb3118f3b
John Gruen <john.gruen@gmail.com>
2015-11-10 20:59:58 +0000

    fix(footer): define URLS

--- idea_town/frontend/static-src/app/templates/footer-view.js
+++ idea_town/frontend/static-src/app/templates/footer-view.js
@@ -2,9 +2,9 @@ export default `
   <div id="main-footer">
     <div class="wrapper">
       <a href="https://www.mozilla.org" class="moz-logo"></a>
-      <a href="https://www.mozilla.org/about/legal/terms/services/" class="boilerplate">Legal</a>
-      <a href="https://www.mozilla.org/privacy/firefox-cloud/" class="boilerplate">Privacy</a>
-      <a href="https://www.mozilla.org/privacy/websites/" class="boilerplate">Cookies</a>
+      <a href="https://www.mozilla.org/about/legal/" class="boilerplate">Legal</a>
+      <a href="https://www.mozilla.org/privacy/" class="boilerplate">Privacy</a>
+      <a href="https://www.mozilla.org/privacy/websites/#cookies" class="boilerplate">Cookies</a>
       <a href="https://www.mozilla.org/contribute/signup/" class="boilerplate">Contribute</a>
     </div>
   </div>
