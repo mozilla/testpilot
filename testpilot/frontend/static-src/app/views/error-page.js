5823cd6ca62e176a8d2c3a0d138615e20c8aeafa
John Gruen <john.gruen@gmail.com>
2016-03-22 17:05:30 -0400

    update modal ui to match spec

--- testpilot/frontend/static-src/app/views/error-page.js
+++ testpilot/frontend/static-src/app/views/error-page.js
@@ -5,11 +5,13 @@ export default PageView.extend({
     <div id="full-page-wrapper" class="space-between blue">
       <header data-hook="header-view"></header>
       <div class="centered-banner">
-        <div id="four-oh-four" class="delayed-fade-in">
-          <h1 data-l10n-id="errorHeading">Whoops!</h1>
-          <p data-l10n-id="errorMessage">Looks like we broke something. Maybe try again later :-\ </p>
+        <div id="four-oh-four" class="modal delayed-fade-in">
+          <h1 data-l10n-id="errorHeading" class="title">Whoops!</h1>
+          <div class="modal-content">
+            <p data-l10n-id="errorMessage">Looks like we broke something. Maybe try again later :-\ </p>
+          </div>
         </div>
-        <div class="copter spin"></div>
+        <div class="copter fade-in-fly-up"></div>
       </div>
       <footer id="main-footer" class="content-wrapper">
         <div data-hook="footer-view"></div>
--- testpilot/frontend/static-src/app/views/not-found-page.js
+++ testpilot/frontend/static-src/app/views/not-found-page.js
@@ -5,13 +5,14 @@ export default PageView.extend({
 <div class="blue">
   <div id="full-page-wrapper" class="centered" data-hook="no-found-page">
     <div class="centered-banner">
-      <div id="four-oh-four" class="delayed-fade-in">
-        <h1 data-l10n-id="notFoundHeader">Four Oh Four!</h1>
-        <div class="modal-controls">
-          <a data-l10n-id="home" class="button default" href="/">Home</a>
+      <div id="four-oh-four" class="modal delayed-fade-in">
+        <h1 data-l10n-id="notFoundHeader" class="title">Four Oh Four!</h1>
+        <br/>
+        <div class="modal-actions">
+          <a data-l10n-id="home" class="button default large" href="/">Home</a>
         </div>
       </div>
-      <div class="copter spin"></div>
+      <div class="copter fade-in-fly-up"></div>
     </div>
   </div>
 </div>
