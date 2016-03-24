e629a45afccafcee8734a080205542340d0a3b47
John Gruen <john.gruen@gmail.com>
2016-03-24 18:58:09 -0400

    obscure site before launch

--- testpilot/frontend/static-src/app/templates/landing-page.js
+++ testpilot/frontend/static-src/app/templates/landing-page.js
@@ -1,105 +1,127 @@
 export default `
 <section data-hook="landing-page">
-  {{^loggedIn}}
-    <div class="blue">
-      <header id="main-header" class="content-wrapper">
-        <h1>
-          <span class="firefox-logo"></span>
-          <a href="/accounts/login/?next=/">
+    {{^loggedIn}}
+        {{^isMoz}}
+          <div id="full-page-wrapper" class="space-between blue">
+            <header id="main-header" class="content-wrapper">
+              <h1>
+                <span class="firefox-logo"></span>
+                <span data-l10n-id="siteName">Firefox Test Pilot</span>
+              </h1>
+              <span/>
+            </header>
+            <div class="centered-banner">
+              <div class="copter-wrapper">
+                <div class="copter fly-down"></div>
+              </div>
+              <h2>Something&#39;s up!</h2>
+            </div>
+            <footer id="main-footer" class="content-wrapper">
+              <div data-hook="footer-view"></div>
+            </footer>
+          </div>
+        {{/isMoz}}
+        {{#isMoz}}
+          <div class="blue">
+            <header id="main-header" class="content-wrapper">
+              <h1>
+                <span class="firefox-logo"></span>
+                <a href="/accounts/login/?next=/">
+                  <span data-l10n-id="siteName">Firefox Test Pilot</span>
+                </a>
+              </h1>
+              {{^isFirefox}}
+                <span/>
+              {{/isFirefox}}
+              {{#isFirefox}}
+                <a data-l10n-id="landingFxaAlternateButton" href="/accounts/login/?next=/" class="button outline">Sign in</a>
+              {{/isFirefox}}
+            </header>
+            <div class="centered-banner">
+              <div class="copter-wrapper">
+                <div class="copter fly-up"></div>
+              </div>
+              <h2>
+                <span data-l10n-id="landingIntroOne">Test out new features.</span>
+                <span data-l10n-id="landingIntroTwo">Give us feedback.</span>
+                <span data-l10n-id="landingIntroThree">Help build Firefox.</span>
+              </h2>
+              {{^isFirefox}}
+                <span data-l10n-id="landingDownloadFirefoxDesc" class="parens">(of course, you'll need Firefox to use Test Pilot)</span>
+                <a href="https://www.mozilla.org/en-US/firefox" class="button primary download-firefox">
+                  <div class="button-icon">
+                    <div class="button-icon-badge"></div>
+                  </div>
+                  <div class="button-copy">
+                    <div data-l10n-id="landingDownloadFirefoxTitle" class="button-title">Firefox</div>
+                    <div data-l10n-id="landingDownloadFirefoxSubTitle" class="button-description">Free Download</div>
+                  </div>
+                </a>
+              {{/isFirefox}}
+              {{#isFirefox}}
+              <a data-l10n-id="landingFxaGetStartedButton" href="/accounts/login/?next=/" class="button large primary">Get started with a Firefox Account</a>
+              {{/isFirefox}}
+            </div>
+            <p data-l10n-id="landingLegalNotice" class="legal-information">By proceeding, you agree to the <a href="https://www.mozilla.org/about/legal/terms/services/">Terms of Service</a> and <a href="https://www.mozilla.org/privacy/firefox-cloud/">Privacy Notice</a> of Test Pilot</p>
+          </div>
+          <div class="content-wrapper">
+              <h2 class="experiment-list-header">Try these features today with Test Pilot</h2>
+              <div data-hook='experiment-list'></div>
+          </div>
+          <div class="blue">
+            <footer id="main-footer" class="content-wrapper">
+              <div class="centered-banner">
+                <div class="copter-wrapper">
+                  <div class="copter"></div>
+                </div>
+                <h2 data-l10n-id="landingPageFooterCopy">More tests coming soon!</h2>
+                {{^isFirefox}}
+                  <span data-l10n-id="landingDownloadFirefoxDesc" class="parens">(of course, you'll need Firefox to use Test Pilot)</span>
+                  <a href="https://www.mozilla.org/en-US/firefox" class="button primary download-firefox">
+                    <div class="button-icon">
+                      <div class="button-icon-badge"></div>
+                    </div>
+                    <div class="button-copy">
+                      <div data-l10n-id="landingDownloadFirefoxTitle" class="button-title">Firefox</div>
+                      <div data-l10n-id="landingDownloadFirefoxSubTitle" class="button-description">Free Download</div>
+                    </div>
+                  </a>
+                {{/isFirefox}}
+                {{#isFirefox}}
+                <a data-l10n-id="landingFxaGetStartedButton" href="/accounts/login/?next=/" class="button large primary">Get started with a Firefox Account</a>
+                {{/isFirefox}}
+              </div>
+              <div data-hook="footer-view"></div>
+            </footer>
+          </div>
+      {{/isMoz}}
+    {{/loggedIn}}
+    {{#loggedIn}}
+      <div id="full-page-wrapper" class="space-between">
+        <header id="main-header" class="content-wrapper">
+          <h1>
+            <span class="firefox-logo"></span>
             <span data-l10n-id="siteName">Firefox Test Pilot</span>
-          </a>
-        </h1>
-        {{^isFirefox}}
-          <span/>
-        {{/isFirefox}}
-        {{#isFirefox}}
-          <a data-l10n-id="landingFxaAlternateButton" href="/accounts/login/?next=/" class="button outline">Sign in</a>
-        {{/isFirefox}}
-      </header>
-      <div class="centered-banner">
-        <div class="copter-wrapper">
-          <div class="copter fly-up"></div>
-        </div>
-        <h2>
-          <span data-l10n-id="landingIntroOne">Test out new features.</span>
-          <span data-l10n-id="landingIntroTwo">Give us feedback.</span>
-          <span data-l10n-id="landingIntroThree">Help build Firefox.</span>
-        </h2>
-        {{^isFirefox}}
-          <span data-l10n-id="landingDownloadFirefoxDesc" class="parens">(of course, you'll need Firefox to use Test Pilot)</span>
-          <a href="https://www.mozilla.org/en-US/firefox" class="button primary download-firefox">
-            <div class="button-icon">
-              <div class="button-icon-badge"></div>
-            </div>
-            <div class="button-copy">
-              <div data-l10n-id="landingDownloadFirefoxTitle" class="button-title">Firefox</div>
-              <div data-l10n-id="landingDownloadFirefoxSubTitle" class="button-description">Free Download</div>
-            </div>
-          </a>
-        {{/isFirefox}}
-        {{#isFirefox}}
-        <a data-l10n-id="landingFxaGetStartedButton" href="/accounts/login/?next=/" class="button large primary">Get started with a Firefox Account</a>
-        {{/isFirefox}}
-      </div>
-      <p data-l10n-id="landingLegalNotice" class="legal-information">By proceeding, you agree to the <a href="https://www.mozilla.org/about/legal/terms/services/">Terms of Service</a> and <a href="https://www.mozilla.org/privacy/firefox-cloud/">Privacy Notice</a> of Test Pilot</p>
-    </div>
-    <div class="content-wrapper">
-        <h2 class="experiment-list-header">Try these features today with Test Pilot</h2>
-        <div data-hook='experiment-list'></div>
-    </div>
-    <div class="blue">
-      <footer id="main-footer" class="content-wrapper">
+          </h1>
+        </header>
         <div class="centered-banner">
           <div class="copter-wrapper">
-            <div class="copter"></div>
+            <div class="copter fly-down"></div>
           </div>
-          <h2 data-l10n-id="landingPageFooterCopy">More tests coming soon!</h2>
-          {{^isFirefox}}
-            <span data-l10n-id="landingDownloadFirefoxDesc" class="parens">(of course, you'll need Firefox to use Test Pilot)</span>
-            <a href="https://www.mozilla.org/en-US/firefox" class="button primary download-firefox">
-              <div class="button-icon">
-                <div class="button-icon-badge"></div>
-              </div>
-              <div class="button-copy">
-                <div data-l10n-id="landingDownloadFirefoxTitle" class="button-title">Firefox</div>
-                <div data-l10n-id="landingDownloadFirefoxSubTitle" class="button-description">Free Download</div>
-              </div>
-            </a>
-          {{/isFirefox}}
-          {{#isFirefox}}
-          <a data-l10n-id="landingFxaGetStartedButton" href="/accounts/login/?next=/" class="button large primary">Get started with a Firefox Account</a>
-          {{/isFirefox}}
+          <h2>
+            <span data-l10n-id="landingInstallHeaderOne">Install the Test Pilot Add-on</span>
+            <span data-l10n-id="landingInstallHeaderTwo">and you're good to go!</span>
+          </h2>
+          <a href="{{ downloadUrl }}" class="button large default">
+            <span data-l10n-id="landingInstallButton">Install the Add-on</span>
+            <div class="state-change-inner no-display"></div>
+          </a>
         </div>
-        <div data-hook="footer-view"></div>
-      </footer>
-    </div>
-  {{/loggedIn}}
-  {{#loggedIn}}
-    <div id="full-page-wrapper" class="space-between">
-      <header id="main-header" class="content-wrapper">
-        <h1>
-          <span class="firefox-logo"></span>
-          <span data-l10n-id="siteName">Firefox Test Pilot</span>
-        </h1>
-      </header>
-      <div class="centered-banner">
-        <div class="copter-wrapper">
-          <div class="copter fly-down"></div>
-        </div>
-        <h2>
-          <span data-l10n-id="landingInstallHeaderOne">Install the Test Pilot Add-on</span>
-          <span data-l10n-id="landingInstallHeaderTwo">and you're good to go!</span>
-        </h2>
-        <a href="{{ downloadUrl }}" class="button large default">
-          <span data-l10n-id="landingInstallButton">Install the Add-on</span>
-          <div class="state-change-inner no-display"></div>
-        </a>
+        <footer id="main-footer" class="content-wrapper">
+          <div data-hook="footer-view"></div>
+        </footer>
       </div>
-      <footer id="main-footer" class="content-wrapper">
-        <div data-hook="footer-view"></div>
-      </footer>
-    </div>
-  {{/loggedIn}}
+    {{/loggedIn}}
 </section>
 `;
 
--- testpilot/frontend/static-src/app/views/landing-page.js
+++ testpilot/frontend/static-src/app/views/landing-page.js
@@ -3,6 +3,8 @@ import PageView from './page-view';
 import template from '../templates/landing-page';
 import ExperimentListView from './experiment-list-view';
 
+// TODO back out this commit (78bd0a93a1q) before launch.
+import queryString from 'query-string';
 
 export default PageView.extend({
   _template: template,
@@ -12,13 +14,15 @@ export default PageView.extend({
 
   render() {
     const isLoggedIn = !!app.me.user.id;
+    const query = queryString.parse(location.search);
+    this.isMoz = query.hasOwnProperty('butimspecial');
     this.loggedIn = isLoggedIn;
     this.downloadUrl = isLoggedIn && app.me.user.addon.url;
     this.isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
     PageView.prototype.render.apply(this, arguments);
 
-    if (!this.loggedIn) {
+    if (!this.loggedIn && this.isMoz) {
       this.renderSubview(new ExperimentListView({
         loggedIn: this.loggedIn,
         isFirefox: this.isFirefox
