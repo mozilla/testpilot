34010389c0d77f2fd6ac8a6d85849dba58486800
Les Orchard <me@lmorchard.com>
2016-03-28 16:21:45 -0400

    Emit request.summary log entries on every request
    - Switch from simple to json log format on dev
    
    - Middleware to emit request.summary type log events
    
    - Tweaks to settings to use the new middleware
    
    Fixes #541

---
+++ testpilot/base/middleware.py
@@ -0,0 +1,43 @@
+from uuid import uuid1
+import datetime
+
+import logging
+
+
+class RequestSummaryLogger(object):
+    """Emit a request.summary type log entry for every request.
+    https://github.com/mozilla-services/Dockerflow/blob/master/docs/mozlog.md
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger('request.summary')
+
+    def process_request(self, request):
+        request._id = str(uuid1())
+        request._logging_start_dt = datetime.datetime.utcnow()
+        return None
+
+    def _build_extra_meta(self, request):
+        td = datetime.datetime.utcnow() - request._logging_start_dt
+        t = td.total_seconds() * 10**6
+        return {
+            "errno": 0,
+            "agent": request.META.get('HTTP_USER_AGENT', ''),
+            "lang": request.META.get('HTTP_ACCEPT_LANGUAGE', ''),
+            "method": request.method,
+            "path": request.path,
+            "rid": request._id,
+            "t": t,
+            "uid": request.user.is_authenticated() and request.user.id or '',
+        }
+
+    def process_response(self, request, response):
+        extra = self._build_extra_meta(request)
+        self.logger.info('', extra=extra)
+        return response
+
+    def process_exception(self, request, exception):
+        extra = self._build_extra_meta(request)
+        extra.update({"errno": 500, "msg": str(exception)})
+        self.logger.error('', extra=extra)
+        return None
