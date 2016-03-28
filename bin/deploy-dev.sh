34c5386b70185425767fdcec754e494700dea7dc
Les Orchard <me@lmorchard.com>
2016-03-28 14:25:47 -0400

    Move to testpilot.dev.mozaws.net
    - Switch to testpilot.dev.mozaws.net domain alias in addon
    
    - Bug fix to deployment script
    
    Fixes #526

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -2,36 +2,29 @@
 
 SHA1=$1
 
+APP_NAME=testpilot-dev
+ENV_NAME=testpilot-dev
+CONFIG_TEMPLATE_NAME=testpilot-dev-v1
+EB_BUCKET=elasticbeanstalk-us-east-1-927034868273
+DOCKERRUN_FILE=testpilot-dev-Dockerrun.aws.json
+
 # Deploy image to Docker Hub
+# TODO: Not used, for now. Just deploy from latest in Dockerrun.
 # docker tag app:build ${DOCKERHUB_REPO}:$SHA
 # docker push ${DOCKERHUB_REPO}:$SHA
 
+# Generate app bundle from template
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json-template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
 # Create new Elastic Beanstalk version
-APP_NAME=testpilot-dev
-ENV_NAME=testpilot-dev
-EB_BUCKET=elasticbeanstalk-us-east-1-927034868273
-
-APP_BUNDLE=${APP_NAME}-${ENV_NAME}-latest-app.zip
-
-# In case we stop deploying strictly from :latest, we can use <TAG> to specify
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json-template > Dockerrun.aws.json
-
-# HACK: This seems like it should be covered by the v1 configuration template,
-# but this encantation seems necessary to get configuration carried over.
-mkdir -p .ebextensions
-aws s3 cp s3://$EB_BUCKET/resources/templates/$APP_NAME/00-environment.config \
-          .ebextensions/00-environment.config
-zip -r $APP_BUNDLE Dockerrun.aws.json .ebextensions/*.config
-
-aws s3 cp $APP_BUNDLE s3://$EB_BUCKET/$APP_BUNDLE
-
 aws elasticbeanstalk create-application-version \
     --application-name $APP_NAME \
     --version-label $SHA1 \
-    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$APP_BUNDLE
+    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment \
-    --template-name v1 \
+    --template-name $CONFIG_TEMPLATE_NAME \
     --environment-name $ENV_NAME \
     --version-label $SHA1
