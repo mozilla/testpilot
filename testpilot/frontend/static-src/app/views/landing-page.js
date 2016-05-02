901751039f6506cfbb50fbd411c27324277607a6
Dave Justice <davejustishh@gmail.com>
2016-05-02 15:28:43 -0400

    remove querystring gateway for public launch
    - fixes #688
    - remove `query-string` module. (Not sure why we were using, since we get `querystring`
    from browserify.)

--- testpilot/frontend/static-src/app/views/landing-page.js
+++ testpilot/frontend/static-src/app/views/landing-page.js
@@ -3,9 +3,6 @@ import PageView from './page-view';
 import template from '../templates/landing-page';
 import ExperimentListView from './experiment-list-view';
 
-// TODO back out this commit (78bd0a93a1q) before launch.
-import queryString from 'query-string';
-
 export default PageView.extend({
   _template: template,
 
@@ -20,15 +17,13 @@ export default PageView.extend({
 
   render() {
     const isLoggedIn = !!app.me.user.id;
-    const query = queryString.parse(location.search);
-    this.isMoz = ('butimspecial' in query);
     this.loggedIn = isLoggedIn;
     this.addonInstalled = app.me.hasAddon;
     this.isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
     PageView.prototype.render.apply(this, arguments);
 
-    if (!this.loggedIn && this.isMoz) {
+    if (!this.loggedIn) {
       this.renderSubview(new ExperimentListView({
         loggedIn: this.loggedIn,
         isFirefox: this.isFirefox,
