ce17113e3c19b0ef1930c8a2a996649907643cbf
Les Orchard <me@lmorchard.com>
2016-05-12 12:33:29 -0400

    Switch to `exec gunicorn` on prod for better signal handling
    > <mostlygeek> could you make it "exec gunicorn ..." instead
    > <mostlygeek> that will cause bash to replace itself with gunicorn and
    >              we can send SIGHUP/SIGKILL/SIG* to it without any
    >              timeouts
    
    Issue #810

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./bin/run-common.sh
-gunicorn testpilot.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
+exec gunicorn testpilot.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
