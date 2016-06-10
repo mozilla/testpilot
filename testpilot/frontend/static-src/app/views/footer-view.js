75724c968392a03bf681db63b869d0e1a1046643
Peter deHaan <peter@deseloper.com>
2016-06-10 16:14:04 -0700

    Fix some legal links and remove unused gulp-imagemin dep

--- testpilot/frontend/static-src/app/views/footer-view.js
+++ testpilot/frontend/static-src/app/views/footer-view.js
@@ -5,7 +5,7 @@ export default BaseView.extend({
                  <a href="https://www.mozilla.org" class="mozilla-logo"></a>
                  <a data-l10n-id="footerLinkLegal" href="https://www.mozilla.org/about/legal/" class="boilerplate">Legal</a>
                  <a data-l10n-id="footerLinkPrivacy" href="/privacy" class="boilerplate">Privacy</a>
-                 <a data-l10n-id="footerLinkTerms" href="/terms" class="boilerplate">Terms of Use</a>
+                 <a data-l10n-id="footerLinkTerms" href="/terms" class="boilerplate">Terms</a>
                  <a data-l10n-id="footerLinkCookies" href="https://www.mozilla.org/privacy/websites/#cookies" class="boilerplate">Cookies</a>
                  <a data-l10n-id="footerLinkContribute" href="https://www.mozilla.org/contribute/signup/" class="boilerplate">Contribute</a>
              </div>`
