5696ae97ac048cc5e88bed7bcf6212bbe1019988
Les Orchard <me@lmorchard.com>
2016-09-09 13:05:20 -0400

    Issue #1307: Add a loading indicator to experiments list while data is still pending

---
+++ frontend/src/app/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="loader">
+      <div className="loader-bar"></div>
+      <div className="loader-bar"></div>
+      <div className="loader-bar"></div>
+      <div className="loader-bar"></div>
+    </div>
+  );
+}
