3dec4d5725424a6dd198300e6ff5515a53d84162
Les Orchard <lorchard@mozilla.com>
2016-09-15 16:39:43 -0400

    Issue #1351: Switch to Mocha / Chai / Enzyme for testing under React

---
+++ frontend/test/app/config-test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+import config from '../../src/app/config';
+
+describe('app/config', () => {
+  it('should have experimentsURL', () =>
+    expect(config).to.have.property('experimentsURL'));
+  it('should have usageCountsURL', () =>
+    expect(config).to.have.property('usageCountsURL'));
+});
