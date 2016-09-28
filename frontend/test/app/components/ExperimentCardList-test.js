0d0aca3b04d4a88072906e0631b85070ba259224
Chuck Harmston <chuck@mozilla.com>
2016-09-28 09:50:23 -0600

    Makes experiment cards more idiomatic:
    - Uses reselect for sorting and filtering.
    - Restructures experiment store (closes #1373)
    - Uses ES6 class structure for ExperimentCardList component.

---
+++ frontend/test/app/components/ExperimentCardList-test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { assert, expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+import ExperimentCardList from '../../../src/app/components/ExperimentCardList';
+import ExperimentRowCard from '../../../src/app/components/ExperimentRowCard';
+import Loading from '../../../src/app/components/Loading';
+
+
+const _exp = [
+  { slug: 'foo' },
+  { slug: 'bat' }
+];
+
+describe('app/components/ExperimentCardList', () => {
+  it('renders a loading screen of no experiments are available', () => {
+    const wrapper = shallow(<ExperimentCardList experiments={[]} />);
+    expect(wrapper.contains(<Loading />)).to.equal(true);
+  });
+
+  it('renders ExperimentRowCards for each experiment', () => {
+    const wrapper = shallow(<ExperimentCardList experiments={_exp} />);
+    expect(wrapper.find(ExperimentRowCard)).to.have.length(_exp.length);
+  });
+
+  it('respects an exception if specified', () => {
+    const wrapper = shallow(<ExperimentCardList experiments={_exp}
+                                                except={_exp[0].slug} />);
+    expect(wrapper.find(ExperimentRowCard)).to.have.length(_exp.length - 1);
+  });
+});
