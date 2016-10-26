d72427ea4a80516b9160695c87755c772b00ec77
Chuck Harmston <chuck@mozilla.com>
2016-10-26 08:39:51 -0500

    Allow experiments to be blocked from or granted to locales.
    Closes #1602, #1670.

--- frontend/src/app/reducers/browser.js
+++ frontend/src/app/reducers/browser.js
@@ -5,9 +5,11 @@ import { isMinFirefoxVersion } from '../lib/utils';
 const userAgent = navigator.userAgent.toLowerCase();
 const isUserAgentFirefox = userAgent.indexOf('firefox') > -1;
 const isUserAgentMinFirefox = isMinFirefoxVersion(isUserAgentFirefox, userAgent, config.minFirefoxVersion);
+const locale = (navigator.language || '').split('-')[0];
 
 export default handleActions({}, {
   isFirefox: isUserAgentFirefox,
   isMinFirefox: isUserAgentMinFirefox,
-  isDev: config.isDev
+  isDev: config.isDev,
+  locale
 });
--- frontend/test/app/selectors/experiment-test.js
+++ frontend/test/app/selectors/experiment-test.js
@@ -1,65 +1,128 @@
 import { assert } from 'chai';
 import moment from 'moment';
 
-import experimentSelector, { allExperimentSelector, launchedExperimentSelector } from '../../../src/app/selectors/experiment';
+import experimentSelector, {
+  allExperimentSelector,
+  launchedExperimentSelector,
+  l10nSelector,
+  localeSelector,
+  onlyLaunchedExperimentSelector,
+} from '../../../src/app/selectors/experiment';
+
 
 describe('app/selectors/experiment', () => {
-  const _store = (experiments, isDev = false) => ({
-    browser: { isDev: isDev },
-    experiments: { data: experiments }
+
+  describe('launch date-related selectors', () => {
+
+    const _store = (experiments, isDev = false) => ({
+      browser: { isDev: isDev },
+      experiments: { data: experiments }
+    });
+
+    const _exp = [
+      { name: 'foo', order: 1 },
+      { name: 'baz', order: 3, launch_date: moment.utc() + 99999 },
+      { name: 'bat', order: 2, launch_date: moment.utc() - 99999 }
+    ];
+
+    describe('allExperimentSelector', () => {
+      it('should filter to store.experiments.data', () => {
+        const store = _store([_exp[0]]);
+        assert.deepEqual(
+          allExperimentSelector(store),
+          store.experiments.data
+        );
+      });
+
+      it('should sort based on the set order', () => {
+        const store = _store(_exp);
+        assert.deepEqual(
+          allExperimentSelector(store),
+          [_exp[0], _exp[2], _exp[1]]
+        );
+      });
+    });
+
+    describe('onlyLaunchedExperimentSelector', () => {
+      it('should exclude experiments with launch dates in the future', () => {
+        const store = _store(_exp);
+        assert.deepEqual(
+          onlyLaunchedExperimentSelector(store),
+          [_exp[0], _exp[2]]
+        );
+      });
+    });
+
+    describe('launchedExperimentSelector', () => {
+      it('should use allExperimentSelector if dev', () => {
+        const store = _store(_exp, true);
+        assert.deepEqual(
+          allExperimentSelector(store),
+          launchedExperimentSelector(store)
+        );
+      });
+
+      it('should use onlyLaunchedExperimentSelector if not dev', () => {
+        const store = _store(_exp, false);
+        assert.deepEqual(
+          onlyLaunchedExperimentSelector(store),
+          launchedExperimentSelector(store)
+        );
+      });
+    });
+
   });
-  const _exp = [
-    { name: 'foo', order: 1 },
-    { name: 'baz', order: 3, launch_date: moment.utc() + 99999 },
-    { name: 'bat', order: 2, launch_date: moment.utc() - 99999 }
-  ];
 
-  describe('allExperimentSelector', () => {
-    it('should filter to store.experiments.data', () => {
-      const store = _store([_exp[0]]);
-      assert.deepEqual(
-        allExperimentSelector(store),
-        store.experiments.data
-      );
+  describe('language-related selectors', () => {
+
+    const BLOCKED_LANG = 'piglatin';
+    const GRANTED_LANG = 'piratespeak';
+    const OTHER_LANG = 'klingon';
+
+    describe('localeSelector', () => {
+      const store = {
+        browser: {
+          locale: BLOCKED_LANG
+        }
+      };
+
+      it('should filter to store.browser.locale', () => {
+        assert.equal(
+          BLOCKED_LANG,
+          localeSelector(store)
+        );
+      });
     });
 
-    it('should sort based on the set order', () => {
-      const store = _store(_exp);
-      assert.deepEqual(
-        allExperimentSelector(store),
-        [_exp[0], _exp[2], _exp[1]]
-      );
+    describe('l10nSelector', () => {
+      const experiments = [
+        { order: 1 },
+        { order: 2, locale_blocklist: [ BLOCKED_LANG ] },
+        { order: 3, locale_grantlist: [ GRANTED_LANG ] }
+      ];
+
+      it('filters experiments blocklisted for the locale', () => {
+        assert.deepEqual(
+          l10nSelector(BLOCKED_LANG, experiments),
+          [experiments[0]]
+        );
+      });
+
+      it('filters experiments grantlisted for other locales', () => {
+        assert.deepEqual(
+          l10nSelector(OTHER_LANG, experiments),
+          [experiments[0], experiments[1]]
+        );
+      });
+
+      it('keeps experiments grantlisted for the locale', () => {
+        assert.deepEqual(
+          l10nSelector(GRANTED_LANG, experiments),
+          experiments
+        );
+      });
     });
-  });
 
-
-  describe('launchedExperimentSelector', () => {
-    it('should exclude experiments with launch dates in the future', () => {
-      const store = _store(_exp);
-      assert.deepEqual(
-        launchedExperimentSelector(store),
-        [_exp[0], _exp[2]]
-      );
-    });
-  });
-
-
-  describe('experimentSelector', () => {
-    it('should use allExperimentSelector if dev', () => {
-      const store = _store(_exp, true);
-      assert.deepEqual(
-        allExperimentSelector(store),
-        experimentSelector(store)
-      );
-    });
-
-    it('should use launchedExperimentSelector if not dev', () => {
-      const store = _store(_exp, false);
-      assert.deepEqual(
-        launchedExperimentSelector(store),
-        experimentSelector(store)
-      );
-    });
   });
 
 });
