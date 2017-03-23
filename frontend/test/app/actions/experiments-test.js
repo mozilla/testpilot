f38c4adfc0374ee66b17ed24cf86a55a6517e620
Donovan Preston <dpreston@mozilla.com>
2017-03-22 20:12:12 -0400

    Fix #2337 The isomorphic-fetch upgrade changed the response.json() api from synchronously returning and returning a promise :(

---
+++ frontend/test/app/actions/experiments-test.js
@@ -0,0 +1,38 @@
+
+import { expect } from 'chai';
+import fetchMock from 'fetch-mock';
+import config from '../../../src/app/config';
+import sinon from 'sinon';
+
+const USAGE_COUNTS = {count: 50};
+
+describe('app/actions/experiments/setInstalled', () => {
+  let dispatch;
+  let fetchUserCounts;
+
+  beforeEach(() => {
+    fetchMock.get(config.usageCountsURL, USAGE_COUNTS);
+    dispatch = sinon.spy(),
+    fetchUserCounts = require('../../../src/app/actions/experiments').default.fetchUserCounts
+  });
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('should be able to fetch the user counts', (done) => {
+    fetchUserCounts(config.usageCountsURL).then(result => {
+      try {
+        expect(JSON.stringify(result)).to.equal(JSON.stringify({
+          type: 'FETCH_USER_COUNTS',
+          payload: {
+            data: USAGE_COUNTS
+          }
+        }));
+      } catch (e) {
+        return done(e);
+      }
+      done();
+    });
+  });
+})
