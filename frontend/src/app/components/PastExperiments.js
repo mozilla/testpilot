5007b574ae045ca347d325bef380ed5472caef33
Donovan Preston <donovanpreston@gmail.com>
2017-05-17 16:10:09 -0400

    Fix #2448 Remove some inline styles from /experiments, fixing some csp violations.

--- frontend/src/app/components/PastExperiments.js
+++ frontend/src/app/components/PastExperiments.js
@@ -22,16 +22,14 @@ export default class PastExperiments extends React.Component {
       <LayoutWrapper flexModifier="card-list">
         {pastExperiments.length > 0 && !showPastExperiments &&
         <div className={classnames(['button past-experiments', 'outline'])}
-            style={{ margin: '0 auto', display: 'table' }}
             onClick={() => this.setState({ showPastExperiments: true })}
             data-l10n-id="viewPastExperiments">View Past Experiments</div>}
         {showPastExperiments &&
         <div>
           <div className={classnames(['button past-experiments', 'outline'])}
-              style={{ margin: '0 auto', display: 'table' }}
               onClick={() => this.setState({ showPastExperiments: false })}
               data-l10n-id="hidePastExperiments">Hide Past Experiments</div>
-          <div style={{ height: '40px' }}/>
+          <div className="past-experiments-padding" />
           <ExperimentCardList {...this.props} experiments={pastExperiments} eventCategory="HomePage Interactions" />
         </div>}
       </LayoutWrapper>
