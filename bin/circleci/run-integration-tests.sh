547fba0348612d579edf4906fc874454abd55aa1
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-05-26 21:46:59 -0700

    Change bash script to timeout if the server fails to start (#2484)

--- bin/circleci/run-integration-tests.sh
+++ bin/circleci/run-integration-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+end=$((SECONDS+60))
 
 npm start &
 STATIC_SERVER_PID=$!
 
 # Wait until the server is available...
-until $(curl --output /dev/null --silent --head --fail -k https://example.com:8000); do
-    printf '.'; sleep 1
+while [ $SECONDS != $end ]; do
+  curl --output /dev/null --silent --head --fail -k https://example.com:8000;
 done
