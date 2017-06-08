7e583ac1c6132e54b8c4ec17a283b1ef4a5fdb3f
Les Orchard <me@lmorchard.com>
2017-06-08 13:43:54 -0400

    Commands to extract strings, check for unextracted strings on CircleCI
    - Add npm scripts to extract strings and check for new strings
    
    - Include l10n:check in CircleCI frontend tests
    
    - Documentation tweaks
    
    Fixes #1781

--- frontend/tasks/content.js
+++ frontend/tasks/content.js
@@ -13,7 +13,10 @@ const util = require('./util');
 gulp.task('content-build', ['content-experiments-data']);
 
 gulp.task('content-watch', () => {
-  gulp.watch(config.CONTENT_SRC_PATH + '/**/*.yaml', ['content-experiments-data']);
+  gulp.watch(config.CONTENT_SRC_PATH + '/**/*.yaml', [
+    'content-experiments-data',
+    'content-extract-experiment-strings'
+  ]);
 });
 
 gulp.task('content-experiments-data', function generateStaticAPITask() {
@@ -22,11 +25,34 @@ gulp.task('content-experiments-data', function generateStaticAPITask() {
     .pipe(gulp.dest(config.DEST_PATH));
 });
 
-function buildExperimentsData() {
-  const index = {results: []};
+gulp.task('content-extract-strings', ['content-extract-experiment-strings']);
+
+gulp.task('content-extract-experiment-strings', () =>
+  gulp.src(config.CONTENT_SRC_PATH + 'experiments/*.yaml')
+    .pipe(buildExperimentsFTL())
+    .pipe(gulp.dest('./locales/en-US')));
+
+function buildExperimentsFTL() {
   const strings = [];
-  const counts = {};
-  const cssStrings = [];
+
+  function collectEntry(file, enc, cb) {
+    const yamlData = file.contents.toString();
+    const experiment = YAML.parse(yamlData);
+
+    if (!experiment.dev) {
+      findLocalizableStrings(experiment);
+    }
+
+    return cb();
+  }
+
+  function endStream(cb) {
+    this.push(new gutil.File({
+      path: 'experiments.ftl',
+      contents: new Buffer(generateFTL())
+    }));
+    cb();
+  }
 
   function findLocalizableStrings(obj, pieces = [], experiment = null) {
     if (!experiment) {
@@ -54,6 +80,14 @@ function buildExperimentsData() {
       return `${a}\n${b.key} = ${value}`;
     }, '');
   }
+
+  return through.obj(collectEntry, endStream);
+}
+
+function buildExperimentsData() {
+  const index = {results: []};
+  const counts = {};
+  const cssStrings = [];
 
   function collectEntry(file, enc, cb) {
     const yamlData = file.contents.toString();
@@ -91,18 +125,11 @@ function buildExperimentsData() {
     }));
 
     index.results.push(experiment);
-    if (!experiment.dev) {  // Don't collect strings for dev-only experiments
-      findLocalizableStrings(experiment);
-    }
 
     cb();
   }
 
   function endStream(cb) {
-    this.push(new gutil.File({
-      path: 'static/locales/en-US/experiments.ftl',
-      contents: new Buffer(generateFTL())
-    }));
     // These files are being consumed by 3rd parties (at a minimum, the Mozilla
     // Measurements Team).  Before changing them, please consult with the
     // appropriate folks!
