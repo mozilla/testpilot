6439454de8d7f472b18e583e93ae278077cd3d1a
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-06-15 13:45:02 -0700

    Added config to test 3 versions of firefox

--- bin/circleci/run-integration-tests.sh
+++ bin/circleci/run-integration-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 end=$((SECONDS+60))
 
+npm run content
 npm start &
 STATIC_SERVER_PID=$!
-
 # Wait until the server is available...
 while [ $SECONDS != $end ]; do
   curl --output /dev/null --silent --head --fail -k https://example.com:8000;
--- frontend/test/ui/test_homepage.py
+++ frontend/test/ui/test_homepage.py
@@ -22,14 +22,14 @@ def test_install_button_loads(base_url, selenium):
 
 @pytest.mark.nondestructive
 def test_number_of_experiments(base_url, selenium):
-    """Test current number experiments"""
+    """Test current number of experiments"""
     page = Home(selenium, base_url).open()
     url = '{0}/{1}'.format(base_url, 'api/experiments.json')
     # Ping api to get current number of completed experiments
     data = requests.get(url, verify=False).json()
     completed_experiments = len(
         [value for value in data['results'] if 'completed' in value and
-            value['completed'] < str(datetime.utcnow())])
+         value['completed'] < str(datetime.utcnow())])
     # Subtract 1 from the experiments found through the api due to locale
     assert len(page.body.experiments) == int(
         len(data['results']) - completed_experiments) - 1
