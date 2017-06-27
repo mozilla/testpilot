425aea65ce7edebc8891bb1bce8b4104aa060bb3
Donovan Preston <donovanpreston@gmail.com>
2017-06-27 10:49:04 -0400

    Switch ErrorPage to fluent-react

--- frontend/src/app/containers/ErrorPage.js
+++ frontend/src/app/containers/ErrorPage.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { Localized } from 'fluent-react';
 import React from 'react';
 
 import Copter from '../components/Copter';
@@ -22,10 +23,14 @@ export default class ErrorPage extends React.Component {
         <LayoutWrapper flexModifier="column-center">
           <div id="four-oh-four" className="modal centered">
             <header className="modal-header-wrapper neutral-modal">
-              <h1 data-l10n-id="errorHeading" className="modal-header">Whoops!</h1>
+              <Localized id="errorHeading">
+                <h1 className="modal-header">Whoops!</h1>
+              </Localized>
             </header>
             <div className="modal-content">
-              <p data-l10n-id="errorMessage">Looks like we broke something. <br /> Maybe try again later.</p>
+              <Localized id="errorMessage">
+                <p>Looks like we broke something. <br /> Maybe try again later.</p>
+              </Localized>
             </div>
           </div>
           <Copter animation="fade-in-fly-up" />
