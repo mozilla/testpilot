b1b8c19227150af2fdf0dccd2da4ce57991bc4c2
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-06-27 16:29:21 -0700

    Disabled Firefox nightly tests due to regression

--- bin/circleci/test-firefox-release.sh
+++ bin/circleci/test-firefox-release.sh
@@ -3,4 +3,5 @@ set -ex
 
 mozinstall $(ls -t /home/ubuntu/firefox-downloads/firefox/*.tar.bz2 | head -1)
 firefox --version
+export PYTEST_ADDOPTS=--html=integration-test-results/ui-test-release.html
 tox -e ui-tests
