2d63f46e0ce201dd50d345b0de55df76b9c6a158
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-09-11 10:39:29 -0700

    Added install and enable test (#2842)

--- bin/circleci/build-frontend.sh
+++ bin/circleci/build-frontend.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -ex
-if [[ $CIRCLE_BRANCH == 'master' ]]; then
-    NODE_ENV=production ENABLE_PONTOON=1 ENABLE_DEV_CONTENT=1 ENABLE_DEV_LOCALES=1 npm run static
-else
-    NODE_ENV=production ENABLE_PONTOON=0 ENABLE_DEV_CONTENT=0 ENABLE_DEV_LOCALES=0 npm run static
-fi
+NODE_ENV=production ENABLE_PONTOON=1 ENABLE_DEV_CONTENT=1 ENABLE_DEV_LOCALES=1 npm run static
 zip -r $CIRCLE_ARTIFACTS/frontend.zip frontend/build
