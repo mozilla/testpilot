6832a04f2400672fddc7be0ccba71ced7d84f6b9
Donovan Preston <donovanpreston@gmail.com>
2017-09-19 15:12:50 -0400

    Remove an obsolete test

--- frontend/test/ui/test_homepage.py
+++ frontend/test/ui/test_homepage.py
@@ -23,20 +23,6 @@ def test_install_button_loads(base_url, selenium):
     assert page.header.is_install_button_displayed
 
 
-@pytest.mark.nondestructive
-def test_number_of_experiments(base_url, selenium):
-    """Test current number of experiments"""
-    page = Home(selenium, base_url).open()
-    url = '{0}/{1}'.format(base_url, 'api/experiments.json')
-    # Ping api to get current number of completed experiments
-    data = requests.get(url, verify=False).json()
-    # add one for dev experiment
-    completed_experiments = len(
-        [value for value in data['results'] if 'completed' in value and
-         value['completed'] < str(datetime.utcnow())]) + 1
-    assert len(page.body.experiments) == int(
-        len(data['results']) - completed_experiments)
-
 
 @pytest.mark.nondestructive
 def test_experiments_load_correct_pages(base_url, selenium):
