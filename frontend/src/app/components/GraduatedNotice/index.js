1fb6d1aec3107c7fefdc2e1d8b561c77bab78e14
Donovan Preston <donovanpreston@gmail.com>
2017-09-20 21:23:11 -0400

    Add the ! icon; fix lint.

--- frontend/src/app/components/GraduatedNotice/index.js
+++ frontend/src/app/components/GraduatedNotice/index.js
@@ -15,15 +15,15 @@ class GraduatedNoticeButton extends React.Component {
 export default class GraduatedNotice extends React.Component {
   render() {
     return <div className="graduated-notice">
-      <img className="graduated-notice-image" src="/static/images/copter.png" />
+      <img className="graduated-notice-image" src="/static/images/info-16.svg" />
       <div className="graduated-notice-text">
         <h1>
           This experiment has ended
         </h1>
         <p>
           {this.props.graduation_url
-            ? "We have prepared a full graduation report."
-            : "We are working on a full report. Check back soon for the details." }
+            ? 'We have prepared a full graduation report.'
+            : 'We are working on a full report. Check back soon for the details.' }
         </p>
       </div>
         {this.props.graduation_url ? <GraduatedNoticeButton graduation_url={this.props.graduation_url} /> : null}
