ecccc5f454bdb60e46ca822c59db8d9e6e15ad9f
Donovan Preston <donovanpreston@gmail.com>
2017-09-25 16:33:12 -0400

    Add tests.

--- frontend/src/app/components/GraduatedNotice/tests.js
+++ frontend/src/app/components/GraduatedNotice/tests.js
@@ -1,2 +1,33 @@
-/* */
+/* global describe, beforeEach, it */
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import GraduatedNotice from './index';
+
+const TEST_URL = 'https://medium.com/firefox-test-pilot';
+
+describe('app/components/GraduatedNotice', () => {
+  let subject;
+  beforeEach(() => {
+    subject = mount(<GraduatedNotice />);
+  });
+
+  it('should render expected content', () => {
+    expect(subject.find(
+      '.graduated-notice-text h1'
+    ).text()).to.equal('This experiment has ended');
+  });
+
+  it('should link to the graduation url if provided', () => {
+    subject.setProps({
+      graduation_url: TEST_URL
+    });
+
+    expect(subject.find(
+      '.graduated-notice-button'
+    ).props()).to.have.property('href', TEST_URL);
+  });
+});
 
