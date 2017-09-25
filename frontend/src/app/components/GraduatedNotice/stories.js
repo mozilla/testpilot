30dbb2a9404bfd38919c2fc7bbf6559c7a4d2df2
Donovan Preston <donovanpreston@gmail.com>
2017-09-25 15:26:43 -0400

    Add stories.

--- frontend/src/app/components/GraduatedNotice/stories.js
+++ frontend/src/app/components/GraduatedNotice/stories.js
@@ -1,2 +1,19 @@
-/* */
+
+
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import GraduatedNotice from './index';
+
+
+storiesOf('GraduatedNotice', module)
+  .add('base state', () =>
+    <GraduatedNotice />
+  )
+  .add('with graduation url', () =>
+    <GraduatedNotice
+      graduation_url="https://medium.com/firefox-test-pilot"
+    />
+  );
 
