a9241e643cd8d9f5518c166d616b58bb4dfca696
Les Orchard <me@lmorchard.com>
2017-09-28 17:32:15 -0400

    Bugfix for Storybook dynamic require looking for missing stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,11 +5,9 @@ import { configure } from '@storybook/react';
 import '../frontend/build/static/styles/experiments.css';
 import '../frontend/build/static/styles/main.css';
 
-const req = require.context('../frontend/stories', true, /\-story\.jsx?$/);
 const reqInSrcTree = require.context('../frontend/src/app', true, /\/stories.jsx?$/);
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
   reqInSrcTree.keys().forEach((filename) => reqInSrcTree(filename));
 }
 
