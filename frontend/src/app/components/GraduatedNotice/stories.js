c1e2a5989eb328c5dfd2334f9b87efa4552fd3bf
Les Orchard <me@lmorchard.com>
2017-10-03 15:20:04 -0400

    Add some context markup to GraduatedNotice stories

--- frontend/src/app/components/GraduatedNotice/stories.js
+++ frontend/src/app/components/GraduatedNotice/stories.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import GraduatedNotice from './index';
+import LayoutWrapper from '../LayoutWrapper';
 
 
 storiesOf('GraduatedNotice', module)
+  .addDecorator(story =>
+    <div className="blue" style={{ padding: 10 }} onClick={action('click')}>
+      <div className="stars" />
+      <div className="default-background">
+        <section id="experiment-page">
+          <div id="details">
+            <LayoutWrapper helperClass="details-content" flexModifier="details-content">
+              <div className="details-overview">
+                <div className={`experiment-icon-wrapper-dev-example experiment-icon-wrapper`}>
+                  <img className="experiment-icon" src="/static/images/check.png"></img>
+                </div>
+              </div>
+              <div className="details-description">
+                {story()}
+              </div>
+            </LayoutWrapper>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
   .add('base state', () =>
     <GraduatedNotice />
   )
