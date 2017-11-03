890942d56e89773d1bd079e6efde69bfd0635cbe
Donovan Preston <donovanpreston@gmail.com>
2017-11-03 11:57:27 -0400

    Add stories for NewsletterFooter

---
+++ frontend/src/app/components/NewsletterFooter/stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import NewsletterFooter from './index';
+import LayoutWrapper from '../LayoutWrapper';
+
+const baseProps = {
+  getWindowLocation: () =>'https://example.com',
+  sendToGA: () => null,
+  newsletterForm: {
+    failed: false,
+    succeeded: false
+  }
+};
+
+storiesOf('NewsletterFooter', module)
+  .addDecorator(story =>
+    <div className="blue" style={{ padding: 10 }}>
+      <div className="stars" />
+      <LayoutWrapper flexModifier="card-list">
+        {story()}
+      </LayoutWrapper>
+    </div>
+  )
+  .add('base state', () =>
+    <NewsletterFooter
+      {...baseProps}
+    />)
+  .add('failed', () =>
+    <NewsletterFooter
+      {...baseProps}
+      newsletterForm={{
+        failed: true,
+        succeeded: false
+      }}
+    />)
+  .add('succeeded', () =>
+    <NewsletterFooter
+      {...baseProps}
+      newsletterForm={{
+        failed: false,
+        succeeded: true
+      }}
+    />);
