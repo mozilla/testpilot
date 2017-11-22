6e880569164be08192a480c778d0154c015933c5
Donovan Preston <donovanpreston@gmail.com>
2017-11-21 22:24:57 -0500

    Skip this test on non-nightly

--- frontend/test/ui/test_experimentpage.py
+++ frontend/test/ui/test_experimentpage.py
@@ -1,5 +1,4 @@
-
-
+import os
 import pytest
 
 from selenium.webdriver.common.by import By
@@ -8,6 +7,9 @@ from pages.desktop.home import Home
 
 
 @pytest.mark.nondestructive
+@pytest.mark.skipif(
+    os.environ.get('SKIP_INSTALL_TEST') is not None,
+    reason='Skip install on Release and Beta Firefox.')
 def test_experiment_page_sticky_header(base_url, selenium):
     """Test that scrolling down on an experiment page with the
     add-on installed properly makes the header sticky
