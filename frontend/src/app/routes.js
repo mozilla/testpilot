5f4cfc13514f9d1127aec932f5d2515cdf40bfe1
Donovan Preston <donovanpreston@gmail.com>
2018-03-05 11:57:25 -0500

    Possibly work?

--- frontend/src/app/routes.js
+++ frontend/src/app/routes.js
@@ -2,17 +2,26 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import App from "./containers/App";
+import ExperimentPage from "./containers/ExperimentPage";
 import HomePage from "./containers/HomePage";
 import HomePageWithAddon from "./containers/HomePage/HomePageWithAddon";
-import ExperimentPage from "./containers/ExperimentPage";
 
+
+function appFactoryFor(Component) {
+  return function appFactoryForComponent() {
+    return <App>
+      <Component />
+    </App>;
+  };
+}
 
 export function routes() {
   return <BrowserRouter>
     <div>
-      <Route path="/" component={HomePage} />
-      <Route path="/experiments" component={HomePageWithAddon} />
-      <Route path="/experiment/:experimentname" component={ExperimentPage} />
+      <Route path="/" component={appFactoryFor(HomePage)} />
+      <Route path="/experiments" component={appFactoryFor(HomePageWithAddon)} />
+      <Route path="/experiment/:experimentname" component={appFactoryFor(ExperimentPage)} />
     </div>
   </BrowserRouter>;
 }
