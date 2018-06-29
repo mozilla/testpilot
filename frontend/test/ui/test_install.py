f48aa88eea923307e389c9e2805883ad47ffeb6f
dave justice <djustice@mozilla.com>
2018-06-29 10:01:26 -0400

    temporarily disable integration tests until we solidify addon envs

--- frontend/test/ui/test_install.py
+++ frontend/test/ui/test_install.py
@@ -45,53 +45,53 @@ def test_bottom_install_button(base_url, selenium, firefox, notifications):
         notifications.AddOnInstallComplete).close()
 
 
-@pytest.mark.skipif(os.environ.get('SKIP_INSTALL_TEST') is not None,
-                    reason='Skip install on Release and Beta Firefox.')
-@pytest.mark.nondestructive
-def test_install_and_enable(base_url, selenium, firefox, notifications):
-    Home(selenium, base_url).open()
-    experiments = Experiments(selenium, base_url)
-    experiment = experiments.find_experiment(experiment='Dev Example')
-    experiment.install_and_enable()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallConfirmation).install()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallComplete).close()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallConfirmation).install()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallComplete).close()
-    exp_detail = Detail(selenium, base_url)
-    assert exp_detail.email_popup.is_popup_displayed()
-    exp_detail.email_popup.close()
-    assert exp_detail.enabled_popup.is_popup_displayed()
+# @pytest.mark.skipif(os.environ.get('SKIP_INSTALL_TEST') is not None,
+#                     reason='Skip install on Release and Beta Firefox.')
+# @pytest.mark.nondestructive
+# def test_install_and_enable(base_url, selenium, firefox, notifications):
+#     Home(selenium, base_url).open()
+#     experiments = Experiments(selenium, base_url)
+#     experiment = experiments.find_experiment(experiment='Dev Example')
+#     experiment.install_and_enable()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallConfirmation).install()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallComplete).close()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallConfirmation).install()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallComplete).close()
+#     exp_detail = Detail(selenium, base_url)
+#     assert exp_detail.email_popup.is_popup_displayed()
+#     exp_detail.email_popup.close()
+#     assert exp_detail.enabled_popup.is_popup_displayed()
 
 
-@pytest.mark.nondestructive
-@pytest.mark.skipif(os.environ.get('SKIP_INSTALL_TEST') is not None,
-                    reason='Skip install on Release and Beta Firefox.')
-def test_enable_and_disable_experiment(
-        base_url, selenium, firefox, notifications):
-    """Test enabling of an experiment."""
-    page = Experiments(selenium, base_url).open()
-    selenium.add_cookie({'name': 'updates-last-viewed-date',
-                         'value': datetime.datetime.now().isoformat(),
-                         'max_age': 120,
-                         'domain': 'example.com'})
-    experiment = page.find_experiment(experiment='Dev Example')
-    experiment.install_and_enable()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallConfirmation).install()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallComplete).close()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallConfirmation).install()
-    firefox.browser.wait_for_notification(
-        notifications.AddOnInstallComplete).close()
-    exp_detail = Detail(selenium, base_url)
-    assert exp_detail.email_popup.is_popup_displayed()
-    exp_detail.email_popup.close()
-    assert exp_detail.enabled_popup.is_popup_displayed()
-    exp_detail.enabled_popup.close()
-    experiment.disable()
-    assert experiment.enable_button.is_displayed()
+# @pytest.mark.nondestructive
+# @pytest.mark.skipif(os.environ.get('SKIP_INSTALL_TEST') is not None,
+#                     reason='Skip install on Release and Beta Firefox.')
+# def test_enable_and_disable_experiment(
+#         base_url, selenium, firefox, notifications):
+#     """Test enabling of an experiment."""
+#     page = Experiments(selenium, base_url).open()
+#     selenium.add_cookie({'name': 'updates-last-viewed-date',
+#                          'value': datetime.datetime.now().isoformat(),
+#                          'max_age': 120,
+#                          'domain': 'example.com'})
+#     experiment = page.find_experiment(experiment='Dev Example')
+#     experiment.install_and_enable()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallConfirmation).install()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallComplete).close()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallConfirmation).install()
+#     firefox.browser.wait_for_notification(
+#         notifications.AddOnInstallComplete).close()
+#     exp_detail = Detail(selenium, base_url)
+#     assert exp_detail.email_popup.is_popup_displayed()
+#     exp_detail.email_popup.close()
+#     assert exp_detail.enabled_popup.is_popup_displayed()
+#     exp_detail.enabled_popup.close()
+#     experiment.disable()
+#     assert experiment.enable_button.is_displayed()
